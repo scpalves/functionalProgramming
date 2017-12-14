26649d875900e00e4e8b830aa273154da4c0ed95
Kvarnström Mattis <mattis.kvarnstrom@scania.com>
2017-12-14 13:51:47 +0100

    Added a few more examples.

---
+++ c#/funcProg-codeNight/MultiplicationFormatter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace funcProg_codeNight
+{
+    public static class MultiplicationFormatter
+    {
+        private static int _counter;
+        static string Counter(int val) => $"{++_counter} x 2 = {val}";
+
+        public static List<string> Format(List<int> numbers)
+            => numbers
+                .Select(Extensions.MultiplyBy2)
+                .Select(Counter)
+                .ToList();
+
+        public static List<string> ParallelFormat(List<int> numbers)
+            => numbers
+                .AsParallel()
+                .Select(Extensions.MultiplyBy2)
+                .Select(Counter)
+                .ToList();
+    }
+
+    internal static class Extensions
+    {
+        internal static int MultiplyBy2(this int value)
+            => value * 2;
+    }
+}
---
+++ c#/funcProg-codeNight/MultiplicationZipper.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace funcProg_codeNight
+{
+    public static class MultiplicationZipper
+    {
+        static readonly Func<int, int> MultiplyBy2 = x => x * 2;
+        static int _counter;
+
+        private static string Counter(int val) => $"{++_counter} x 2 = {val}";
+
+        public static List<string> ParallelMultiplyBy2(List<int> numbers)
+            => numbers.AsParallel().Select(MultiplyBy2)
+                .Zip(Enumerable.Range(1, numbers.Count).AsParallel(), (val, count) => $"{count} x 2 = {val}")
+                .ToList();
+    }
+}
